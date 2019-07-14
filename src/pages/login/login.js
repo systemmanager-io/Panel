a600e81a46fd6871c9640573cc5ed080b69964ee
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2019-07-14 02:15:03 +0200

    Changed login to be black as well

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import LoginLayout from "../../layouts/LoginLayout";
 import {Link} from "react-router-dom";
+import Card from "../../components/components/card";
 
 export default class Login extends Component {
 
@@ -14,50 +15,47 @@ export default class Login extends Component {
     render() {
         return (
             <LoginLayout>
-                <div className="bg-gray-700 h-screen font-sans">
+                <div className="bg-gray-800 h-screen font-sans">
                     <div className="container mx-auto h-full flex justify-center items-center">
-                        <div className="w-full sm:w-6/12 lg:w-4/12">
-                            <div className="p-8 bg-white mb-6 rounded-lg shadow-lg">
-                                <h1 className="font-hairline text-4xl mb-6 font-bold text-center">SystemManager<span
-                                    className="text-base align-text-top">v0.1</span></h1>
-                                <div hidden={this.state.waitingForToken}>
-                                    <div className="mb-4">
-                                        <label className="font-bold text-grey-darker block mb-2">Email</label>
-                                        <input type="text"
-                                               className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-                                               placeholder="Your Username"/>
-                                    </div>
-
-                                    <div className="mb-4">
-                                        <label className="font-bold text-grey-darker block mb-2">Password</label>
-                                        <input type="text"
-                                               className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-                                               placeholder="Your Password"/>
-                                    </div>
+                        <Card className="p-8 mb-6" color="">
+                            <h1 className="font-hairline text-4xl mb-6 font-bold text-center">SystemManager<span
+                                className="text-base align-text-top">v0.1</span></h1>
+                            <div hidden={this.state.waitingForToken}>
+                                <div className="mb-4">
+                                    <label className="font-bold text-grey-darker block mb-2">Email</label>
+                                    <input type="text"
+                                           className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
+                                           placeholder="Your Username"/>
                                 </div>
 
-                                <div hidden={!this.state.waitingForToken}>
-                                    Loading
+                                <div className="mb-4">
+                                    <label className="font-bold text-grey-darker block mb-2">Password</label>
+                                    <input type="text"
+                                           className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
+                                           placeholder="Your Password"/>
                                 </div>
-
-                                <div className="flex items-center justify-between">
-                                    <button
-                                        onClick={this.submitLogin.bind(this)}
-                                        className="bg-gray-800 hover:bg-teal text-white w-full font-bold py-2 px-4 rounded">
-                                        Login
-                                    </button>
-                                </div>
-                                <div className="flex items-center justify-between">
-                                    <Link
-                                        disabled={true}
-                                        className="no-underline inline-block align-baseline font-bold text-sm text-blue hover:text-blue-dark float-right"
-                                        to="/forgot_password">
-                                        Forgot Password?
-                                    </Link>
-                                </div>
-
                             </div>
-                        </div>
+
+                            <div hidden={!this.state.waitingForToken}>
+                                Loading
+                            </div>
+
+                            <div className="flex items-center justify-between">
+                                <button
+                                    onClick={this.submitLogin.bind(this)}
+                                    className="bg-gray-800 hover:bg-teal text-white w-full font-bold py-2 px-4 rounded">
+                                    Login
+                                </button>
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <Link
+                                    disabled={true}
+                                    className="no-underline inline-block align-baseline font-bold text-sm text-blue hover:text-blue-dark float-right"
+                                    to="/forgot_password">
+                                    Forgot Password?
+                                </Link>
+                            </div>
+                        </Card>
                     </div>
                 </div>
             </LoginLayout>
